ed77060c0b644641a81f00f4eef26d947f69e6e7
Djibril Develop <djibrildevelop@Djibs-MacBook-Pro.local>
2024-06-16 01:01:20 +0300

    -continue

--- app/main.ts
+++ app/main.ts
@@ -5,13 +5,14 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
   //handle incoming requests and the rest
-//   socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
+  //   socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
   socket.on("data", (data) => {
-    console.log(data.toString(), "data string");
     const requestPath = data.toString().split(" ")[1];
 
     const response =
-      requestPath === "/" ? "HTTP/1.1 200 OK\r\n\r\n" : 'HTTP/1.1 404 Not Found\r\n\r\n';
+      requestPath === "/"
+        ? "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 7\r\n\r\ndjibril"
+        : "HTTP/1.1 404 Not Found\r\n\r\n";
     socket.write(Buffer.from(response));
   });
 
